4494d617ca52477c03ed833a98b9a596c2a33ba0
Satsuki Akiba <anosatsuk124@gmail.com>
2022-04-11 12:26:19 +0000

    tokenize with reguler expression

--- reference-implementation/src/tokenizer.rs
+++ reference-implementation/src/tokenizer.rs
@@ -1,4 +1,5 @@
-use std::panic;
+use regex::Regex;
+use std::{fmt::format, panic};
 
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub enum Token {
@@ -32,178 +33,47 @@ impl Tokenizer {
         }
     }
 
-    fn peek(&self) -> Option<char> {
-        self.input.chars().nth(self.pos)
-    }
+    pub fn tokenize(&self) -> Option<Token> {
+        let index = r"([a-z]+)";
+        let predicate_name = r"([A-Z]+\w*)";
+        let sentence_def = r"(.+)";
+        let atom = r"\w+";
+        let comma = r"\s*,\s*";
+        let colon = r"\s*:\s*";
+        let def_op = r"\s*->\s*";
+        let curly_bracket = (r"\s*\{\s*", r"\s*\}\s*");
+        let arg = format!(r"(\w+)");
+        let args = format!(r"(\(({Arg}{Comma})*{Arg}\s*)\)", Arg = arg, Comma = comma);
+        let predicate = format!(
+            r"({PredicateName}{Args})",
+            PredicateName = &predicate_name,
+            Args = &args,
+        );
+        let sentence = format!(
+            r"({Predicate}{DefOp}{OpenCurlyBracket}(({Index}{Colon}{SentenceDef})+{Comma})*({Index}{Colon}{SentenceDef}){CloseCurlyBracket}|{Predicate}{DefOp}{SentenceDef})",
+            Predicate = &predicate,
+            DefOp = &def_op,
+            OpenCurlyBracket = &curly_bracket.0,
+            CloseCurlyBracket = &curly_bracket.1,
+            SentenceDef = &sentence_def,
+            Index = &index,
+            Colon = &colon,
+            Comma = &comma,
+        );
+        let sentences = format!(
+            r"(Sentence{OpenCurlyBracket}({Sentence}{Comma})*{Sentence}+{CloseCurlyBracket})",
+            Sentence = &sentence,
+            OpenCurlyBracket = &curly_bracket.0,
+            CloseCurlyBracket = &curly_bracket.1,
+            Comma = &comma,
+        );
+        let program = sentences.clone();
 
-    fn pop(&mut self) -> Option<char> {
-        self.pos += 1;
-        self.input.chars().nth(self.pos - 1)
-    }
+        println!("{}", sentences);
+        let re = Regex::new(&*sentences).unwrap();
+        let text = &*self.input;
 
-    pub fn tokenize(&mut self) -> Option<Token> {
-        loop {
-            match self.peek() {
-                None => return None,
-                Some(c) => match c {
-                    'A'..='Z' => return self.tokenize_str(),
-                    c if c.is_ascii() => {
-                        self.pop();
-                        return self.tokenize_sentence();
-                    }
-                    c if c.is_whitespace() => self.skip_whitespace(),
-                    c => panic!("Unexpected char: {}:{}", c, self.pos),
-                },
-            }
-        }
-    }
-
-    fn tokenize_dict(&mut self, v: &mut Vec<Token>) -> Option<Token> {
-        while let Some(c) = self.peek() {
-            let mut s = String::new();
-            match c {
-                '{' => {
-                    self.pop();
-                    continue;
-                }
-                ':' | '"' => {
-                    v.push(self.tokenize_str().unwrap());
-                    self.pop();
-                    return self.tokenize_dict(v);
-                }
-                '}' | ',' => {
-                    return Some(Token::DictValue(v.to_vec()));
-                }
-                'a'..='z' => {
-                    s.push(c);
-                    self.pop();
-                    while let Some(c @ 'a'..='z') = self.peek() {
-                        self.pop();
-                        s.push(c);
-                    }
-                    return Some(Token::DictName(s.to_string()));
-                }
-                c if c.is_whitespace() => self.skip_whitespace(),
-                _ => panic!("Unexpected char: {}:{}", c, self.pos),
-            }
-        }
-        None
-    }
-
-    fn tokenize_sentence(&mut self) -> Option<Token> {
-        while let Some(c) = self.peek() {
-            match c {
-                '-' | '+' => {
-                    return self.tokenize_op();
-                }
-                '{' | ':' | ',' => {
-                    return self.tokenize_dict(&mut Vec::new());
-                }
-                '}' | ')' => break,
-                // '(' | ',' => {
-                //     break;
-                // }
-                '(' => {
-                    break;
-                }
-                '"' | 'A'..='z' => {
-                    return self.tokenize_str();
-                }
-                c if c.is_whitespace() => self.skip_whitespace(),
-                c => panic!("Unexpected char: {}:{}", c, self.pos),
-            }
-        }
-        None
-    }
-
-    fn skip_whitespace(&mut self) {
-        while let Some(c) = self.peek() {
-            match c {
-                c if c.is_whitespace() => {
-                    self.pop();
-                }
-                _ => {
-                    break;
-                }
-            }
-        }
-    }
-
-    fn tokenize_op(&mut self) -> Option<Token> {
-        if let Some(c) = self.peek() {
-            match c {
-                '-' => {
-                    self.pop();
-                    if let Some('>') = self.peek() {
-                        return Some(Token::Op("->".to_string()));
-                    }
-                }
-                '+' => {
-                    return Some(Token::Op(c.to_string()));
-                }
-                _ => {
-                    panic!("Unexpected char: {}:{}", c, self.pos);
-                }
-            }
-        }
-        None
-    }
-
-    fn tokenize_str(&mut self) -> Option<Token> {
-        if let Some(c) = self.peek() {
-            let mut s = String::new();
-            match c {
-                '"' => {
-                    self.pop();
-                    while let Some(c) = self.peek() {
-                        match c {
-                            '"' => {
-                                return Some(Token::SentenceDef(s));
-                            }
-                            _ => {
-                                s.push(c);
-                                self.pop();
-                            }
-                        }
-                    }
-                }
-                'A'..='Z' => {
-                    s.push(c);
-                    self.pop();
-                    while let Some(c @ 'a'..='z') = self.peek() {
-                        self.pop();
-                        s.push(c);
-                    }
-                    match &*s {
-                        "Sentence" => {
-                            self.pop();
-                            return Some(Token::Op(s.to_string()));
-                        }
-                        "Main" => {
-                            self.pop();
-                            return Some(Token::Op(s.to_string()));
-                        }
-                        _ => return Some(Token::PredicateName(s.to_string())),
-                    }
-                }
-                'a'..='z' => {
-                    while let Some(c) = self.peek() {
-                        match c {
-                            'a'..='z' => {
-                                s.push(c);
-                                self.pop();
-                            }
-                            _ => {
-                                return Some(Token::Args(s));
-                            }
-                        }
-                    }
-                }
-                _ => {
-                    panic!("Unexpected char: {}:{}", c, self.pos);
-                }
-            }
-        }
+        println!("{:?}", re.captures(text));
         None
     }
 
